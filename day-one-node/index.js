ec21fc46f49ef19f8ddf2bd55a92b4547c2be9b3
Eliza-collab <hanenbadradam@gmail.com>
2026-09-14 20:07:20 +0200

    console.log("My favorite languages are: JavaScript & C#."); console.log("I want to learn Software Development to improve my practical skills.");

--- day-one-node/index.js
+++ day-one-node/index.js
@@ -1,3 +1,5 @@
 console.log("My name is Fatima Adam.");
 console.log("I'm learning Node.js.");
 console.log("Day 1 is completed!.");
+console.log("My favorite programming language are: JavaScript & C#.");
+console.log("I want to learn Software Development to improve my practical skills.");
